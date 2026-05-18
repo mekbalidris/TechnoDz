53876eed63a72b7826367b382affb6bf2f69f8e5
rachid <salhinerachid@gamil.com>
2026-05-19 00:18:42 +0100

    fix BASE_URL

--- includes/config.php
+++ includes/config.php
@@ -3,6 +3,6 @@ define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'techno_dz');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('BASE_URL', '/nexus_shop'); // change if served from a different path
+define('BASE_URL', '/TechnoDZ'); // change if served from a different path
 define('PRODUCT_IMG_DIR', __DIR__ . '/../assets/images/products');
 define('PRODUCT_IMG_URL', BASE_URL . '/assets/images/products');
